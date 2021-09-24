7964a0f025368e58a6db899079a60823da55270c
WebTorill <46745387+WebTorill@users.noreply.github.com>
2021-09-24 09:10:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DebuggingExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool numberNotFound = false;
+            int number = 2520;
+
+            while(numberNotFound)
+            {
+                for(int divider = 0; divider < 20; divider++)
+                {
+                    if(number % divider != 0)
+                        break;
+                    else
+                        numberNotFound = true;
+                }
+                number += 2;
+            }
+
+            // 232792600 er svaret vi leder efter
+            Console.WriteLine(number);
+
+            Console.ReadLine();
+        }
+    }
+}
